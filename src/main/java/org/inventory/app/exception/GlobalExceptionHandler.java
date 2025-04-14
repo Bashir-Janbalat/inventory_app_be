a5124eedad0eca7e27b8d1cddd89c00576338a58
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-04-14 23:28:20 +0200

    add new error response class

--- src/main/java/org/inventory/app/exception/GlobalExceptionHandler.java
+++ src/main/java/org/inventory/app/exception/GlobalExceptionHandler.java
@@ -7,26 +7,33 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("error", "Not Found");
-        body.put("message", ex.getMessage());
-        body.put("path", request.getRequestURI());
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        String timestamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+        errorResponse.setTimestamp(timestamp);
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setError("Not Found");
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setPath(request.getRequestURI());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> handleUserAlreadyExists(UserAlreadyExistsException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExists(UserAlreadyExistsException ex) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        String timestamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
+        errorResponse.setTimestamp(timestamp);
+        errorResponse.setStatus(HttpStatus.CONFLICT.value());
+        errorResponse.setError("User Already Exists!!");
+        errorResponse.setMessage(ex.getMessage() + " Please try again with a different username.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 }
