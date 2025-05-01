3594d09ae522dadd7ae9226f7a87a062bce84768
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-05-01 18:03:57 +0200

    Add unique constraint to Brand name field

--- src/main/java/org/inventory/app/model/Brand.java
+++ src/main/java/org/inventory/app/model/Brand.java
@@ -1,10 +1,7 @@
 package org.inventory.app.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +13,7 @@ public class Brand {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     public Brand(String name) {
