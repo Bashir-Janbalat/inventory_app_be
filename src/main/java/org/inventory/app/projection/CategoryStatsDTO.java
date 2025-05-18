ac2be08888489cb692fbef549fcc5896f2a5413c
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-05-18 15:14:44 +0200

    add default constructor to CategoryStatsDTO

--- src/main/java/org/inventory/app/projection/CategoryStatsDTO.java
+++ src/main/java/org/inventory/app/projection/CategoryStatsDTO.java
@@ -2,11 +2,13 @@ package org.inventory.app.projection;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryStatsDTO implements Serializable {
 
 
